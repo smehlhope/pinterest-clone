fdb14f441c793ad32ce431db4286c865d33b4bc0
smehlhope <stephanie.mehlhope@gmail.com>
2016-03-03 14:42:03 -0800

    Add and configure acts_as_votable

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
 
   def index
   	#reverse order of pins - newest at top
@@ -37,6 +37,12 @@ class PinsController < ApplicationController
   def destroy
   	@pin.destroy
   	redirect_to root_path
+  end
+
+  def upvote
+    #pin found by before_action
+    @pin.upvote_by current_user
+    redirect_to :back
   end
 
   private
